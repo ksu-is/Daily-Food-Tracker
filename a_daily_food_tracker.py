00e8aecb86e9a9eb100bcac51ff16c6149b32394
Ameen877 <aomitogu@students.kennesaw.edu>
2024-11-20 20:56:28 -0500

    Added lunch meal to the list and printed it.

--- a_daily_food_tracker.py
+++ a_daily_food_tracker.py
@@ -3,8 +3,14 @@
 # A list to store meals
 meals = []
 
-
 meals.append("Breakfast: Eggs and toast")
 
 # Printing the meals
 print(meals)
+
+# Adding another meal 
+meals.append("Lunch: Sandwich")
+
+# Printing the updated list 
+print(meals)
+
